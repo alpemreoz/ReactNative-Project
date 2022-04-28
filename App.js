e52872eb2cfd90d631bb74362e5a886b3219e60f
Alp Emre Öztürk <ozturkalpemre@gmail.com>
2022-04-28 23:29:50 +0300

    title(Tinder Gold)

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import data from "./assets/data.json";
+import Title from "./title";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!!</Text>
-      <Text>{data[0].title}</Text>
+    <SafeAreaView style={styles.container}>
+      {/* <Text>Hello World</Text> */}
+      {/* <Text>{data[0].title}</Text> */}
+      <Title/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginTop: 30,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    
   },
 });
---
+++ title.js
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
+import React from "react";
+
+const DATA = [
+  {
+    id: 1,  
+    title: "Tinder Gold",
+    subTitle: "Get %50 off your first 2 month",
+    day: "Today",
+    oldprice: "was 520",
+    newprice: "was 270",
+    feature: ["unlimited liked", "see who likes you", "boost your profile"],
+    caption: "cancel anytime ,recurring billing, reviwes at full price",
+    time: "offer ends in 23:59:16",
+    button: "continue",
+  },
+];
+
+export default function title() {
+
+
+  return (              
+    <SafeAreaView >
+      <FlatList 
+      data={DATA}
+      keyExtractor={index =>index.id}
+      renderItem={({item})=>
+        (   
+            
+            <SafeAreaView>
+                <Text>{item.title}</Text>
+                
+            </SafeAreaView>
+            
+
+        )}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
