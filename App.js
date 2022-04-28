28d868d667622261943c1261019cec3967385766
Alp Emre Öztürk <ozturkalpemre@gmail.com>
2022-04-28 11:19:30 +0300

    tittle to app.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import data from "./assets/data.json";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!!!!!</Text>
+      <Text>{data[0].title}</Text>
       <StatusBar style="auto" />
     </View>
   );
